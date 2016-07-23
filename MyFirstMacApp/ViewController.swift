71e66c944b08e3ed8e0a13f3db1b4ab6c6296cca
codeRomeo <tariq@tariqs-macbook-pro.local>
2016-07-23 15:23:43 -0400

    Initial Commit

---
+++ MyFirstMacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyFirstMacApp
+//
+//  Created by Tariq Khalid on 7/23/16.
+//  Copyright © 2016 Tariq Khalid. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
